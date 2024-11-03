add1240670e149e63f9963e6067db1255b519415
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-03 11:12:20 +0500

    .

--- lab02.py
+++ lab02.py
@@ -11,3 +11,17 @@
 # a = 2 % 2 + 2 * 2 - 2 / 2 
 # print (a)
 
+# b = 3 / 2 + 5 * 4 / 3
+
+# print (b)
+
+# Area of circle
+# r = 50
+# pi= 3.142
+# area = pi * r**2
+# print (area)
+
+# print (50 + 4)
+# print (50 - 4)
+# print (50 * 4)
+# print (50 / 4)
