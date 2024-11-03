7535563d41535599fc675e7477408e773df5ce97
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-03 12:07:34 +0500

    .

---
+++ lab03.py
@@ -0,0 +1,9 @@
+# print("Hello \b World!")
+
+# first_number = str ( input ("Enter first number") )
+# second_number = str ( input ("Enter second number") )
+
+# sum = (first_number + second_number)
+# print("Addition of two number is: ", sum)
+
+
