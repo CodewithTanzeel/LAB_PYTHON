54a4ceaab256c93b5205236593e8ef57cc30e842
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-03 10:48:08 +0500

    .

---
+++ lab01.py
@@ -0,0 +1,9 @@
+# My First Code
+# print ("\n Welcome to the Ubisoft universal studios")
+#My second Program
+# print(" Welcome  in the \n world of programming ")
+# BioData
+
+print ("My Name is Tanzeel Ahmad")
+print ("I am currently pursuing SoftwareEngineering at SSUET")
+print ("I am familiar with JAVASCRIPT , TypeScript and PYTHON")
