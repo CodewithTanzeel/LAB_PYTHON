271142b1f0cb0869304e2e4d6a41792bdd98c943
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-18 22:59:42 +0500

    lab04 completed

--- lab04.py
+++ lab04.py
@@ -18,18 +18,36 @@
 # < 10	Bachelors	70,000
 
 
-qualification_user = input("Enter your Qualification :   ").lower()
-years_of_service = int(input("Input Your Years of Service :"))
+# qualification_user = input("Enter your Qualification :   ").lower()
+# years_of_service = int(input("Input Your Years of Service :"))
 
-if (qualification_user == "bachelors" and years_of_service >= 10 ):
-    print("Your estimated Salary will be 100,000")
-elif (qualification_user == "masters" and years_of_service >= 10 ):
-    print("Your estimated Salary will be 150,000")
+# if (qualification_user == "bachelors" and years_of_service >= 10 ):
+#     print("Your estimated Salary will be 100,000")
+# elif (qualification_user == "masters" and years_of_service >= 10 ):
+#     print("Your estimated Salary will be 150,000")
     
-elif (qualification_user == "masters" and years_of_service < 10 ):
-    print("Your estimated Salary will be 100,000")
+# elif (qualification_user == "masters" and years_of_service < 10 ):
+#     print("Your estimated Salary will be 100,000")
     
-elif (qualification_user == "bachelors" and years_of_service < 10 ):
-    print("Your estimated Salary will be 70,000")
+# elif (qualification_user == "bachelors" and years_of_service < 10 ):
+#     print("Your estimated Salary will be 70,000")
+# else:
+#     print ("invalid inputs")
+
+
+
+
+
+age = int(input("Enter the person's age: "))
+if age < 2:
+    print("The person is a baby.")
+elif 2 <= age < 4:
+    print("The person is a toddler.")
+elif 4 <= age < 13:
+    print("The person is a kid.")
+elif 13 <= age < 20:
+    print("The person is a teenager.")
+elif 20 <= age < 65:
+    print("The person is an adult.")
 else:
-    print ("invalid inputs")
+    print("The person is an elder.")
