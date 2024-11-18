4485d075e79c7aed2d15f16dfd9d482d17c582c3
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-18 23:29:51 +0500

    task 2

--- lab05.py
+++ lab05.py
@@ -5,16 +5,25 @@
 #       The first 10 natural number are :
 #       1 2 3 4 5 6 7 8 9 10 
 #       The Sum is : 55
-sum = 0
-num = 1
-for num in range (1,11):
-     print ( num , end ="")
-     sum += num
+# sum = 0
+# num = 1
+# for num in range (1,11):
+#      print ( num , end ="")
+#      sum += num
 
 
-print("\nThe Sum is:", sum)
+# print("\nThe Sum is:", sum)
  
 
      
-     
+# 2.	Write a program to print the multiplication table of the number entered by the user. The table should get displayed in the following form.
+# 29 x 1 =  29
+# 29 x 2 =  58
+# …
+# 29 x 10 = 290
+
+num = int(input("Enter a input for a table u want to print : "))
+for i in range(1,11):
+    print (num, "X", i , "=" , num * i)
+
  
