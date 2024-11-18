9414fccbb333687113f34f8e6f05c02eba235c6f
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-18 23:24:20 +0500

    task01

---
+++ lab05.py
@@ -0,0 +1,20 @@
+# C.   Write Python programs for the following:
+
+# 1.	Write a program that prints the first 10 natural numbers and their sum using ‘for loop’.
+# Sample output:
+#       The first 10 natural number are :
+#       1 2 3 4 5 6 7 8 9 10 
+#       The Sum is : 55
+sum = 0
+num = 1
+for num in range (1,11):
+     print ( num , end ="")
+     sum += num
+
+
+print("\nThe Sum is:", sum)
+ 
+
+     
+     
+ 
