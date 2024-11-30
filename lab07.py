43b7234851ab78d38c2f020ed89b3b7090e37f1c
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-12-01 00:22:45 +0500

    .

---
+++ lab07.py
@@ -0,0 +1,13 @@
+# define sub(x, y) 
+# return x + y
+
+# define describe_pet(pet_name, animal_type='dog'): 
+# print("\nI have a " , animal_type ,".")
+# print("My " , animal_type + "'s name is " , pet_name +
+# ".")
+
+def type_of_int(i):
+     if i // 2 == 0:
+       return 'even'
+     else:
+       return 'odd'
