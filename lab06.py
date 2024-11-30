2278a53ee5a01c4d34b4164e1e331c20f671a774
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-11-30 23:46:39 +0500

    F

--- lab06.py
+++ lab06.py
@@ -1,38 +1,4 @@
-# 1.Write a program to add first seven terms twice of the following series:
-# Program to add first seven terms twice of a series
-result = 0
-for i in range(1, 8):
-    factorial = 1
-    for j in range(1, i + 1):
-        factorial *= j
-    result += i / factorial
-result *= 2
-print("Result:", result)
-
-
-
-# 2.Write a program to print all prime numbers from 900 to 1000. [Hint: Use nested loops, break and continue]
-
-for num in range(900, 1001):
-    for i in range(2, num):
-        if num % i == 0:
-            break
-    else:
-        print(num, end=" ")
-
-
-# # 3.Write a program to display multiplication table (1-5) using nested looping Sampled output: [hint: '{ } ' .format(value)]
-# # 02 X 01 = 02
-
-for i in range(1, 6):
-    for j in range(1, 11):
-        print(f"{i} x {j} = {i * j}")
-    print()
-
-
-#Exercise A
-
-prompt = "\nPlease enter the name of a city you have visited:" "\n(Enter 'quit' when you are finished.)"
+prompt = "\nPlease enter the name of a city you have visited:" prompt+="\n(Enter 'quit' when you are finished.)"
 while True:
 city = str(input(prompt))if city == quit:
 break; else:
@@ -41,25 +7,71 @@ print("I'd love to go to " , city.title() , "!")
 
 
 
-if x>2:
-if y>2:
-z=x+y
-print(“z is”, y)else:
-print(“x is”, x)
+
+
+
+# # 1.Write a program to add first seven terms twice of the following series:
+# # Program to add first seven terms twice of a series
+# result = 0
+# for i in range(1, 8):
+#     factorial = 1
+#     for j in range(1, i + 1):
+#         factorial *= j
+#     result += i / factorial
+# result *= 2
+# print("Result:", result)
+
+
+
+# # 2.Write a program to print all prime numbers from 900 to 1000. [Hint: Use nested loops, break and continue]
+
+# for num in range(900, 1001):
+#     for i in range(2, num):
+#         if num % i == 0:
+#             break
+#     else:
+#         print(num, end=" ")
+
+
+# # # 3.Write a program to display multiplication table (1-5) using nested looping Sampled output: [hint: '{ } ' .format(value)]
+# # # 02 X 01 = 02
+
+# for i in range(1, 6):
+#     for j in range(1, 11):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+
+# #Exercise A
+
+# prompt = "\nPlease enter the name of a city you have visited:" "\n(Enter 'quit' when you are finished.)"
+# while True:
+# city = str(input(prompt))if city == quit:
+# break; else:
+# print("I'd love to go to " , city.title() , "!")
+
+
+
+
+# if x>2:
+# if y>2:
+# z=x+y
+# print(“z is”, y)else:
+# print(“x is”, x)
 
 
 
 
 
-balance = int(input("enter yourbalance1:"))while true: if balance <=9000:
-continue;
-balance = balance+999.99 print("Balance is", balance)
+# balance = int(input("enter yourbalance1:"))while true: if balance <=9000:
+# continue;
+# balance = balance+999.99 print("Balance is", balance)
 
 
 
-i = 10
-if (i==10):
-   print ("i is smaller than 15")# Nested - if statement
-   print ("i is smaller than 12 too")
-else:
-   print ("i is greater than 15")
+# i = 10
+# if (i==10):
+#    print ("i is smaller than 15")# Nested - if statement
+#    print ("i is smaller than 12 too")
+# else:
+#    print ("i is greater than 15")
