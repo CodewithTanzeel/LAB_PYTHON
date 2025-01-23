efa7ec804b6a7e9a3c71e09d122a4c81e793151e
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-01-24 00:34:21 +0500

    .

---
+++ LAB 14.py
@@ -0,0 +1,46 @@
+# class A():
+#     def __init__(self, i):
+#         self.i = i
+# def main():
+#     a = A()
+#     print(a.i)
+# main() # Call the main function
+
+
+# class Dog:  
+#     attr1 = "mamal"
+#     def fun(self):
+#         print("I'm a", self.attr1)
+
+
+# class Car():
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#     def get_descriptive_name(self):
+#         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+#         return long_name.title()
+# my_new_car = Car('audi', 'a4', 2016)
+# print(my_new_car.get_descriptive_name())
+
+
+# class Person:  
+#     # init method or constructor   
+#     def __init__(self, name):  
+#         self.name = name  
+    
+#     def say_hi(self):  
+#         print('Hello, my name is', self.name)  
+    
+# p = Person('XXX')  
+# p.say_hi()  
+
+
+# class Dog:  
+#     attr1 = "mamal"
+#     def fun(self):
+#         print("I'm a", self.attr1)
+
+
+print("Hello \b  World!")
